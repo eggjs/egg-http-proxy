9a71d1da7d64ba446c76444d577f665e6438205b
TZ | 天猪 <atian25@qq.com>
2020-03-11 11:23:40 +0800

    fix: should not set content-type when body (#3)

--- config/config.default.js
+++ config/config.default.js
@@ -24,7 +24,6 @@ exports.httpProxy = {
     'keep-alive': true,
     'proxy-authenticate': true,
     'proxy-authorization': true,
-    'content-length': true,
     te: true,
     trailer: true,
     'transfer-encoding': true,
